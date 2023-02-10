2295f9b5cf49840d99a1f50d238540157067dc62
Adhyan Gupta <gadhyan250@gmail.com>
2023-02-10 20:50:54 +0530

    static taskbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import { useEffect, useState } from 'react';
-
+import TextField from "@mui/material/TextField";  // added
 import AllStocks from './pages/AllStocks';
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Stock from './pages/Stock';
@@ -10,7 +10,18 @@ function App() {
 
 
   return (
+    
     <div className="App">
+      <center>
+      <div className="search">
+        <TextField
+          id="outlined-basic"
+          variant="outlined"
+          fullWidth
+          label="Search"
+        />
+        </div>
+        </center>
       <BrowserRouter>
         <Routes>
           <Route path='/' element={<AllStocks />}></Route>
