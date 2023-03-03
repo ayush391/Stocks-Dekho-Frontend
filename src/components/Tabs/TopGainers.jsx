2f4aa7321b9f47ce9f2ba49f1ce4cf725b4279e9
ayush391 <stringakaak@gmail.com>
2023-03-03 12:22:43 +0530

    update: create a general stock table component

--- src/components/Tabs/TopGainers.jsx
+++ src/components/Tabs/TopGainers.jsx
@@ -6,6 +6,8 @@ import { Box } from '@mui/system';
 import StockSymbol from '../Table/StockSymbol';
 import StockPChange from '../Table/StockPChange';
 import StockChange from '../Table/StockChange';
+import TableSkeleton from '../Loading/TableSkeleton';
+import StockTable from '../Table/StockTable';
 
 const BASE_URL = process.env.REACT_APP_BASE_URL + '/prices/top-gainers'
 const LIMIT = 10
@@ -43,41 +45,11 @@ const TopGainers = () => {
 
 
     return (
+        <>
+            <StockTable stocksData={stocksData} />
 
-        <TableContainer component={Card} variant='outlined'>
-            <Table>
-                <TableBody>
-                    {
-                        stocksData.map((stock, idx) => {
-                            return (
-                                <TableRow key={stock.symbol}>
-                                    <TableCell>
-                                        <Box sx={{ display: 'flex', gap: 1, fontWeight: 'bold', }}>
-                                            <StockSymbol symbol={stock.symbol} idx={idx} />
-                                            <Typography
-                                                sx={{
-                                                    whiteSpace: 'nowrap', textOverflow: 'ellipsis', overflow: 'hidden'
-                                                }}
-                                            >
-                                                {stock.meta ? stock.meta.companyName : stock.symbol}
-                                            </Typography>
-                                        </Box>
-                                    </TableCell>
-                                    <TableCell align="right">₹{parseFloat(stock.lastPrice).toFixed(2)}</TableCell>
-                                    <TableCell align="right"><StockChange change={stock.change} /></TableCell>
-                                    <TableCell align="right">
-                                        <StockPChange pChange={stock.pChange} />
-                                    </TableCell>
-                                </TableRow>
-                            )
-                        }
-                        )
-                    }
-                </TableBody>
-            </Table>
-
-        </TableContainer>
+        </>
     )
 }
 
-export default TopGainers
+export default React.memo(TopGainers)
