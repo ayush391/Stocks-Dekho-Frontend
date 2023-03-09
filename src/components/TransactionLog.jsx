d9168a8977299571b6a541387cedc4e369431d9d
DeepakSingh260 <alexmha260@gmail.com>
2023-03-09 15:52:28 +0530

    transaction page and rerouting

--- src/components/TransactionLog.jsx
+++ src/components/TransactionLog.jsx
@@ -3,12 +3,42 @@ import { Box, Button, Card, CardContent, CardHeader, Container, Divider, Modal, 
 export const TransactionLogCard=()=>{
     return(
         <Container>
-
+            
             <Card>
-                <CardContent>
+                <CardContent >
+                <Stack direction={'row'} justifyContent={'space-evenly'}>
                     <Typography>Stock Symbol</Typography>
+                    <Typography >QTY</Typography>
+
+                    <Typography >Total Price</Typography>
+                    
+                </Stack>
                 </CardContent>
             </Card>
+            <Card>
+                <CardContent >
+                <Stack direction={'row'} justifyContent={'space-evenly'}>
+                    <Typography>INFY</Typography>
+                    <Typography >5</Typography>
+
+                    <Typography sx={{border:'1px solid', borderRadius:1 , paddingLeft:2 , paddingRight:2 , color:'green'}}>6500</Typography>
+                    
+                </Stack>
+                </CardContent>
+            </Card>
+            <Card>
+                <CardContent >
+                <Stack direction={'row'} justifyContent={'space-evenly'}>
+                    <Typography>WIPRO</Typography>
+                    <Typography >3</Typography>
+
+                    <Typography sx={{border:'1px solid', borderRadius:1 , paddingLeft:2 , paddingRight:2 , color:'green'}}>1289</Typography>
+                    
+                </Stack>
+                </CardContent>
+            </Card>
+            
+           
 
         </Container>
     )
--- src/pages/TransactionHistory.jsx
+++ src/pages/TransactionHistory.jsx
@@ -4,6 +4,7 @@ import { TabPanel } from "../components/Tabs/TabPanel"
 import {useState} from 'react'
 import {Typography} from '@mui/material'
 import { TransactionLogCard } from "../components/TransactionLog"
+import {Link} from 'react-router-dom'
 export const TransactionHistory=()=>{
     const [value, setValue] = useState(0);
     const handleChange = (event, newValue) => {
@@ -12,7 +13,7 @@ export const TransactionHistory=()=>{
 
     return(
         <div style={{width:'100%'}}>
-            <Typography textAlign={'center' } marginBottom={1} sx={{backgroundColor:' #c3defd' , fontSize:22 , padding:1,borderRadius:3 , color:'white'}}>Transaction History</Typography>
+            <Typography component={Link} to='/' textAlign={'center' } marginBottom={1} sx={{backgroundColor:' #c3defd' , fontSize:22 ,borderRadius:3 , color:'white' , width:'100%' , display:'flex' , justifyContent:'center'}}>Transaction History</Typography>
             <AntTabs  style={{width:'100%'}} value={value} onChange={handleChange} aria-label="basic tabs example" sx={{ marginBottom: 2 }}>
                 <AntTab style={{width:'50%'}} label="Buy" />
                 <AntTab style={{width:'50%'}} label="Sell" />
