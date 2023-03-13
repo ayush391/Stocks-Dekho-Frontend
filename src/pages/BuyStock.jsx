7bba496c118fd72fd73436d2081d131047a8dd4a
ayush391 <stringakaak@gmail.com>
2023-03-13 14:32:38 +0530

    update: updated ui for buy stock

--- src/pages/BuyStock.jsx
+++ src/pages/BuyStock.jsx
@@ -1,16 +1,4 @@
-import {
-  Button,
-  ChakraProvider,
-  FormLabel,
-  Input,
-  Select,
-  Stat,
-  StatArrow,
-  StatGroup,
-  StatHelpText,
-  StatNumber
-} from '@chakra-ui/react';
-import { Typography } from '@mui/material';
+import { Box, Button, Container, Stack, TextField, Typography } from '@mui/material';
 import axios from 'axios';
 import { getAuth } from 'firebase/auth';
 import * as React from 'react';
@@ -26,7 +14,6 @@ export default function BuyStock(props) {
   const { symbol } = params;
 
   const { stockData, loading, error } = useStockData(symbol);
-  // const stockPrice = useStockPrice(symbol)
 
   const [stockQty, setStockQty] = useState(1);
   const user = getAuth(app);
@@ -45,89 +32,66 @@ export default function BuyStock(props) {
     setOrderReview(response.data);
     setOpen(true);
   };
-  const StockVal = () => {
-    return (
-      <div style={{ marginLeft: 26 }}>
-        <StatGroup>
-          <Stat>
-            <StatNumber>{stockData != null ? stockData.lastPrice : 2452}</StatNumber>
-            <StatHelpText>
-              <StatArrow
-                type={
-                  stockData != null && stockData.pChange.substring(0, 1) == '-'
-                    ? 'decrease'
-                    : 'increase'
-                }
-              />
-              {stockData != null ? stockData.pChange : 2452}
-            </StatHelpText>
-          </Stat>
-        </StatGroup>
-      </div>
-    );
-  };
 
   const handleQty = (e) => {
     setStockQty(e.target.value);
   };
 
   return (
-    <div style={{ marginLeft: 'auto', marginRight: 'auto' }}>
-      {error ? (
-        <Typography>An error occured</Typography>
-      ) : loading ? (
-        <CircularLoading />
-      ) : (
-        <>
-          <div style={{ display: 'flex', flexDirection: 'row' }}>
-            <h1 style={{ textAlign: 'initial', fontSize: 22, fontWeight: 'bold', margin: 10 }}>
-              {'Price'}
-            </h1>
+    <Container maxWidth="sm">
+      <Box
+        sx={{
+          textAlign: 'center'
+        }}>
+        {error ? (
+          <Typography>An error occured</Typography>
+        ) : loading ? (
+          <CircularLoading />
+        ) : (
+          <Stack direction="column" alignItems="center">
+            <Box marginY={2}>
+              <img width={150} src={stockData?.icon}></img>
 
-            <ChakraProvider>
-              <StockVal />
-            </ChakraProvider>
-          </div>
-          <div style={{ display: 'flex', flexDirection: 'row' }}>
-            <FormLabel style={{ marginRight: 10, fontWeight: 'bold', fontSize: 22 }}>QTY</FormLabel>
-            <Input
-              onChange={handleQty}
-              endDecorator={
-                <React.Fragment>
-                  <Select
-                    variant="plain"
-                    value={'INR'}
-                    sx={{ mr: -1.5, '&:hover': { bgcolor: 'transparent' } }}></Select>
-                </React.Fragment>
-              }
-              sx={{ width: 300 }}
+              <Typography>{'Total Amount'}</Typography>
+              <Typography variant="h1" fontWeight="bold" color="primary">
+                ₹{stockData ? (stockData.lastPrice * stockQty).toFixed(2) : 2452}
+              </Typography>
+              <Typography variant="body1" color="grey" gutterBottom>
+                1 Stock ≈ ₹{stockData ? stockData.lastPrice.toFixed(2) : 2452}
+              </Typography>
+
+              <TextField
+                fullWidth
+                autoFocus
+                placeholder="Quantity"
+                type="number"
+                inputProps={{ style: { textAlign: 'center', fontSize: '2rem' }, min: '1' }}
+                onChange={handleQty}
+                sx={{
+                  marginY: 5,
+                  fontSize: '5rem'
+                }}
+              />
+            </Box>
+
+            <Button
+              size="large"
+              disabled={stockQty <= 0}
+              variant="contained"
+              fullWidth
+              onClick={BuyReview}>
+              Buy
+            </Button>
+            <ConfirmOrder
+              onClose={() => setOpen(false)}
+              open={open}
+              icon={stockData != null ? stockData.icon : 'Symbol'}
+              reviewOrder={orderReview}
+              transactionType="buy"
             />
-          </div>
-          <p>
-            Buying {stockQty} stock : {stockData != null ? stockData.symbol : 'Symbol'} at{' '}
-            {stockData != null ? stockData.lastPrice * stockQty : 2452}
-          </p>
-          <Button
-            style={{
-              width: '100%',
-              marginRight: 'auto',
-              marginLeft: 'auto',
-              color: 'white',
-              backgroundColor: 'green'
-            }}
-            onClick={BuyReview}>
-            {' '}
-            Buy {props.symbol}{' '}
-          </Button>
-          <ConfirmOrder
-            onClose={() => setOpen(false)}
-            open={open}
-            icon={stockData != null ? stockData.icon : 'Symbol'}
-            reviewOrder={orderReview}
-            transactionType="buy"
-          />
-        </>
-      )}
-    </div>
+          </Stack>
+        )}
+      </Box>
+    </Container>
   );
 }
