4db40e02b320bf2098435705a30663e587cb5808
ayush391 <stringakaak@gmail.com>
2023-03-30 11:58:59 +0530

    fix: cleaned up code

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,22 +9,16 @@ const Home = () => {
       <Container maxWidth="sm">
         <StockPanel />
       </Container>
-      {/* <Box
-        sx={{
-          backgroundImage: `url(${bgPatternPanel})`,
-          backgroundSize: '40%'
-        }}> */}
+
       <Container
         maxWidth="sm"
         sx={{
           my: 2,
           py: 2
-          // backdropFilter: 'blur(2px)'
         }}>
         <SectorTab />
         <News />
       </Container>
-      {/* </Box> */}
     </>
   );
 };
