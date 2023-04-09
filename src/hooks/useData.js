c3d40aae213aa402cb2fe6788f6b0bbf08da4b96
AnkushChauhan-Pluang <ankush.chauhan@pluang.com>
2023-04-09 14:39:51 +0530

    feat: improved search using react-query and debounce

--- src/hooks/useData.js
+++ src/hooks/useData.js
@@ -2,18 +2,22 @@ import { useQuery } from 'react-query';
 import { api } from '../services';
 import { useEffect, useState } from 'react';
 
-export const useData = (route, queryKey = [], query) => {
+export const useData = (route, queryKey = [], query, searchQuery) => {
   const [ready, setReady] = useState(false);
 
   useEffect(() => {
-    if (queryKey.every((part) => !!part)) {
-      setReady(true);
+    if (route && queryKey.every((part) => !!part)) {
+      setReady(queryKey.includes('search') ? !!searchQuery : true);
     }
-  }, [queryKey]);
+  }, [queryKey, searchQuery]);
 
   const fetchData = () => {
-    return api.get(`${route}/${queryKey.join('/')}`, { ...query });
+    const params = searchQuery ? { symbol: searchQuery, ...query } : { ...query };
+    return api.get(`${route}/${queryKey.join('/')}`, params);
   };
 
-  return useQuery(queryKey, fetchData, { enabled: ready, select: (data) => data.data });
+  return useQuery([...queryKey, searchQuery], fetchData, {
+    enabled: ready,
+    select: (data) => data.data
+  });
 };
