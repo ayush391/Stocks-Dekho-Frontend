59d81bb7e874b9e3167b02cf8dfc538af9c8fc3f
AnkushChauhan-Pluang <ankush.chauhan@pluang.com>
2023-04-27 22:46:29 +0530

    fix: login/logout in sidebar

--- src/components/SidebarMenu/index.jsx
+++ src/components/SidebarMenu/index.jsx
@@ -3,14 +3,17 @@ import Box from '@mui/material/Box';
 import { useAppContext } from '../../context/AppState';
 import SidebarMenuItem from './SidebarMenuItem';
 import { menuItems } from './SidebarMenuItem/menuItems';
+import { signOut } from 'firebase/auth';
 
 const SidebarMenu = () => {
-  const { user } = useAppContext();
+  const { auth, user } = useAppContext();
+
+  const logout = () => signOut(auth);
 
   return (
     <Box sx={{ width: 250 }} role="presentation">
       <List>
-        {menuItems(user).map((item, idx) => {
+        {menuItems(user, logout).map((item, idx) => {
           if (item.divider) return <Divider key={idx} sx={{ my: 1 }} />;
           if (item.visible === undefined || item.visible)
             return <SidebarMenuItem key={item.name} item={item} />;
